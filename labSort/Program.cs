798f3968d4d48cd2a42f1c1059b0018bb1861c28
stu-itu120 <stu-itu120@hq.corp.vlsu.ru>
2022-03-10 16:52:02 +0300

    Добавьте файлы проекта.

---
+++ labSort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace labSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = ArrayGenerator.GenerateInt(5, 12, 23, 0.5, 2);
+          // array = Quicksort.Sort(array);
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine(array[i]);
+        }
+    }
+}
---
+++ labSort/Quicksort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labSort
+{
+    internal class Quicksort
+    {
+
+
+        public static int[] Sort(int[] array)
+        {
+            int temp;
+            for (int i = 0; i < array.Length; i++)
+                for (int j = i; j > 0; j--)
+                {
+                    if (array[j] < array[j-1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j-1];
+                        array[j-1] = temp;
+                    }
+                }
+            return array;
+        }
+
+
+        public static double[] Sort(double[] array)
+        {
+            double temp;
+            for (int i = 0; i < array.Length; i++)
+                for (int j = i; j > 0; j--)
+                {
+                    if (array[j] < array[j - 1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+                    }
+                }
+            return array;
+        }
+
+   
+    }
+}
