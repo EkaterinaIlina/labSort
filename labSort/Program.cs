c38836bd90a677537dede476a20383eb3a0ca942
stu-itu120 <stu-itu120@AMDVLA3100W01>
2022-03-10 17:02:34 +0300

    сортировка вставками

--- labSort/Program.cs
+++ labSort/Program.cs
@@ -7,7 +7,10 @@ namespace labSort
         static void Main(string[] args)
         {
             int[] array = ArrayGenerator.GenerateInt(5, 12, 23, 0.5, 2);
-          // array = Quicksort.Sort(array);
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine(array[i]);
+            Console.WriteLine();
+            array = Quicksort.Sort(array);
             for (int i = 0; i < array.Length; i++)
                 Console.WriteLine(array[i]);
         }
